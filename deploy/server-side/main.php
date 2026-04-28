8e8f95579a605cbd5d9d310dcc7b172cfddeb7ee
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-04-28 20:28:04 +0300

    deploy: made POC for hostfly

---
+++ deploy/server-side/main.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Access-Control-Allow-Origin: http://localhost:8080');
+
+$curPollConfig = json_decode(
+  file_get_contents('../../curPollConfig.json'), true
+);
+$allTopics = json_decode(
+  file_get_contents('data/topics.json'), true
+);
+
+$frontendData = [
+  'topics' => $allTopics[$curPollConfig['topics']],
+  'group' => $curPollConfig['group']
+];
+
+echo json_encode($frontendData, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
