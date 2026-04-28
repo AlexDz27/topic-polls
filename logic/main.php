bbdfaf9fb0025ece3dfb59abd05eb21eb1a06bba
Alex Dzyuba <alexeydzyuba27@gmail.com>
2026-04-28 19:06:34 +0300

    w

--- logic/main.php
+++ logic/main.php
@@ -3,4 +3,11 @@
 $curPollConfig = json_decode(
   file_get_contents('../curPollConfig.json'), true
 );
+$allTopics = json_decode(
+  file_get_contents('../data/topics.json'), true
+);
 
+$frontendData = [
+  'topics' => $allTopics[$curPollConfig['topics']],
+  'group' => $curPollConfig['group']
+];
